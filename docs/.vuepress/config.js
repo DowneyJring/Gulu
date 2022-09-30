a5fc8b9fb2605f964ef9394ed94eb12e806df5c3
RubyJag <tkeai527@yeah.net>
2022-09-30 14:00:20 +0800

    首次部署，激活Github Action

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,14 +4,29 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "Gulu",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/Gulu/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
 
   head,
-  plugins,
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "f2182596f7e1bf853d43",
+        clientSecret: "ff8a750e85ef45edc4432784b1a3ed552ec97c11",
+        owner: "RubyJag527",
+        repo: "Gulu",
+      },
+    ],
+  ],
   themeConfig,
 }
